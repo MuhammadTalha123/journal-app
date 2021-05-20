fb78dd05b4d991fb11b65ab1d05e3d22ec9a3496
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-05-21 00:49:50 +0500

    complete al functionality of nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from "react"; 
+import React from "react";
 import "./App.css";
+import { Router, Switch, Route } from "react-router-dom";
+import PrimarySearchAppBar from "./components/navbar/Navbar";
+import About from "./components/about/About";
+import AuthorGuidelines from "./components/authorGuidelines/AuthorGuidelines";
+import SubmitAPaper from "./components/submit A Paper/SubmitAPaper";
+import AimAndScope from "./components/aim&scope/AimAndScope";
+import Contact from "./components/contact/Contact";
+import history from "./components/history/history";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <PrimarySearchAppBar />
+      <div>
+        <Router history={history}>
+          <Switch>
+            <Route exact path="/aimandscope" component={AimAndScope} />
+            <Route exact path="/submitapaper" component={SubmitAPaper} />
+            <Route
+              exact
+              path="/authorguidelines"
+              component={AuthorGuidelines}
+            />
+            <Route exact path="/" component={About} />
+            <Route exact path="/contact" component={Contact} />
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
