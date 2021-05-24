949660a97a5abdccb8149c2a65f64075346a9a96
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-05-25 00:31:09 +0500

    update contact style

--- src/components/submit A Paper/SubmitAPaper.js
+++ src/components/submit A Paper/SubmitAPaper.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import "./submitAPaper.css";
 
 const SubmitAPaper = () => {
     return (
-        <div>
+        <div className="submit_paper_container">
             <h1>Submit A Paper</h1>
+            <div className="submission_div">
+                <h3>Submission</h3>
+                <p><b>Authors are requested to submit their papers electronically by e-mail attachments to the Editorial : ijafms@gmail.com.</b> The attached files should be in MS Word, WordPerfect or RTF format. Any supplemental files should also be in Microsoft Word, RTF, Word Perfect, or Excel format.<b> PDFs are not acceptable.</b> Authors are requested to submit the text, tables, and artwork in electronic form to this address. The Publisher and Editor regret that they are not able to consider submissions that do not follow these procedures. All correspondence, including notification of the Editor's decision and requests for revision, takes place by e-mail.</p>
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
         </div>
     );
 };
